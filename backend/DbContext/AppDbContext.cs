ac4060f83c8933de6fb4286eae7011d62f9d505f
rick-23 <swamynathan.radhakrishnan@gmail.com>
2024-01-08 13:33:52 -0500

    Added DBMigrations and AppDBContext for SQLite db

--- backend/DbContext/AppDbContext.cs
+++ backend/DbContext/AppDbContext.cs
@@ -1,8 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 
 public class ApplicationDBContext : DbContext {
-    public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { 
 
+    protected readonly IConfiguration Configuration;
+
+    public ApplicationDBContext(IConfiguration configuration) { 
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
     }
 
     public DbSet<Employee> Employees { get; set; }
---
+++ backend/Migrations/20240108182809_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DateOfJoining = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    EmployeeID = table.Column<string>(type: "TEXT", nullable: false),
+                    PhoneNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    UserName = table.Column<string>(type: "TEXT", nullable: false),
+                    WorkStation = table.Column<string>(type: "TEXT", nullable: false),
+                    IdCardNumber = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
